fbf8e113d197124f71b827bfbda7535ddf210c85
tgutierrez <tomas.gutierrez.zamorano@gmail.com>
2024-04-23 21:30:00 -0400

    - Added Testing Project - Added Simple Test Harness to simplify tests

---
+++ Testing/Integration/FhirDispatcher/CanPerformQueryById.cs
@@ -0,0 +1,68 @@
+﻿using Lumbre.Middleware.Services.Definition;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lumbre;
+using Lumbre.Middleware;
+using Hl7.Fhir.Model;
+using Lumbre.Interfaces.Contracts;
+using Task = System.Threading.Tasks.Task;
+using Testing.SimplePersistanceHarness;
+
+namespace Testing.Integration.Query
+{
+    [TestClass]
+    public class CanPerformQueryById
+    {
+        public readonly IFhirDispatcher _dispatcher;
+        public readonly IServiceCollection _services;
+        public readonly SimpleTestHarness _simpleDatabase;
+
+        public CanPerformQueryById()
+        {
+            _services = new ServiceCollection()
+                    .AddLumbre(cfg =>
+                     {
+                         cfg.AddSimpleTestHarnes();
+                         //cfg.UseMongoDb(conn =>
+                         //{
+                         //    conn.DatabaseName = "FHIR";
+                         //    conn.ConnectionString = "mongodb://localhost:27017/";
+                         //});
+                     });
+
+            var builder = _services.BuildServiceProvider();
+
+            _dispatcher = builder.GetService<IFhirDispatcher>();
+        }
+
+        [TestMethod]
+        public async Task Get_Result_As_Object()
+        {
+            var result = await _dispatcher.GetObjectById<Patient>("1");
+
+            Assert.IsNotNull(result);
+
+            Assert.IsInstanceOfType<ValidResponse<ObjectResponse<Patient>>> (result);
+            Assert.IsNotNull(result.Response.Value);
+            Assert.AreEqual("1", result.Response.Value.Id);
+        }
+
+        [TestMethod]
+        public async Task Get_Result_As_JsonString()
+        {
+            var result = await _dispatcher.GetJsonById<Patient>("1");
+
+            Assert.IsNotNull(result);
+
+            Assert.IsInstanceOfType<ValidResponse<JsonResponse>>(result);
+            Assert.IsNotNull(result.Response.Value);
+            Assert.IsTrue(!String.IsNullOrEmpty(result.Response.Value));
+        }
+
+    }
+}
---
+++ Testing/SimplePersistanceHarness/SimpleTestHarness.cs
@@ -0,0 +1,68 @@
+﻿using Lumbre.Interfaces.Common;
+using Lumbre.Interfaces.Contracts;
+using Lumbre.Interfaces.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lumbre.Interfaces.Common.Primitives;
+
+namespace Testing.SimplePersistanceHarness
+{
+    /// <summary>
+    /// Simple test harness 
+    /// </summary>
+    public class SimpleTestHarness : IRepository
+    {
+        private readonly Dictionary<CollectionName, Dictionary<ResourceId, JsonPayload>> _data;
+
+        public SimpleTestHarness() {
+            _data = new Dictionary<CollectionName, Dictionary<ResourceId, JsonPayload>>
+            {
+                { new CollectionName("Patient"), new Dictionary<ResourceId, JsonPayload>() }
+            };
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var jsonInput = File.ReadAllText("SimplePersistanceHarness\\BaseTestData\\Patient.json");
+            _data[new CollectionName("Patient")].Add(new ResourceId("1"), new JsonPayload(jsonInput));
+        }
+
+        public Dictionary<CollectionName, Dictionary<ResourceId, JsonPayload>> Data => _data;
+
+
+        public Task<IRepositoryResult> ReadById(Primitives.CollectionName collectionName, Primitives.ResourceId resourceId)
+        {
+            if (_data[collectionName].TryGetValue(resourceId, out var result))
+            {
+                return Task.FromResult(new Results(result) as IRepositoryResult);
+            }
+
+            return Task.FromResult(new NotFound() as IRepositoryResult);  
+        }
+            
+
+        public Task Upsert(Primitives.CollectionName collection, Primitives.ResourceId resourceId, Primitives.JsonPayload payload)
+        {
+            _data[collection].Add(resourceId, payload);
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class Extension
+    {
+        public static IConfigurator AddSimpleTestHarnes(this IConfigurator cfg)
+        {
+            cfg.Services.AddTransient<IRepository, SimpleTestHarness>();
+            cfg.Services.AddTransient<SimpleTestHarness>(); // Register itself so we can access it's properties
+            return cfg;
+        }
+    }
+}
